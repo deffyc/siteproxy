7593c94353a416e330bd773c72114a07ccfba4b4
deffyc <deffyc@users.noreply.github.com>
2020-09-28 23:34:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,41 @@ const middle1 = (req, res, next) => {
     }
     next()
 }
+
+// Authenticator
+app.use(function(req, res, next) {
+    var auth;
+
+    // check whether an autorization header was send    
+    if (req.headers.authorization) {
+      // only accepting basic auth, so:
+      // * cut the starting "Basic " from the header
+      // * decode the base64 encoded username:password
+      // * split the string at the colon
+      // -> should result in an array
+      auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+      // use Buffer.from in with node v5.10.0+ 
+      // auth = Buffer.from(req.headers.authorization.substring(6), 'base64').toString().split(':');
+    }
+
+    // checks if:
+    // * auth array exists 
+    // * first value matches the expected user 
+    // * second value the expected password
+    if (!auth || auth[0] !== process.env["OAUTH_CLIENT_ID"] || auth[1] !== process.env["OAUTH_CLIENT_SECRET"]) {
+        // any of the tests failed
+        // send an Basic Auth request (HTTP Code: 401 Unauthorized)
+        res.statusCode = 401;
+        // MyRealmName can be changed to anything, will be prompted to the user
+        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+        // this will displayed in the browser when authorization is cancelled
+        res.end('Unauthorized');
+    } else {
+        // continue with processing, user was authenticated
+        next();
+    }
+});
+
 app.use(middle1)
 app.use(proxy)
 
